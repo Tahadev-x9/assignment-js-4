37788721c49a03e2a1bdcfff6d2415fdb38e567b
TAHA <dg0882318@gmail.com>
2026-05-20 22:50:10 +0500

    Done

--- app.js
+++ app.js
@@ -1,2 +1,63 @@
-alart("hello world")
+// ASSIGNMENT JS 4
 
+
+
+// 1 - VARIABLE [ LET , VAR , CONST]
+
+// ANS1
+// var         re-assign        block scope
+// let         re-assign        block scope 
+// const       not-reassign     function scope 
+
+// ANS2
+let userName = "hassan";
+console.log(userName)
+
+// ANS3
+//  Error ata hai type error 
+
+// ANS4
+var a = 10
+var a = 20
+// output 20   bcz var declare kia ja skata hai  
+console.log(a)
+
+// ANS5
+//  Const array 
+//  array kai andr values change hosakti hai. 
+// const arr = [1 , 2 ];
+//   arr.push(3);
+
+// ANS6
+let name = "hassan";
+let age = "21";
+let city = "karachi ";
+
+console.log("My name is hassan ans i live in karachi ");
+
+
+
+//  2 - STRING METHODS
+
+// ANS1
+// .toUpperCase() → text ko CAPITAL mein convert karta hai.
+// .toLowerCase() → text ko small letters mein convert karta hai.
+
+// ANS3
+let str = "javascript";    // OUTPUT JAVA 
+console.log(str.slice(0, 4));
+
+// ANS4
+// .charAt()
+// str.charAt(0)
+// Indexing
+// str[0]
+
+// Difference:
+
+// charAt() method hai.
+// Indexing direct access hai.
+
+// ANS5
+let user = "hAsAn";
+console.log(user.toLowerCase());
